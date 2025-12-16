c6b3d286301493f22cd142b7c11e75fe8d953517
Arthur <arthureduardo355@gmail.com>
2025-12-16 12:18:41 -0300

    Primeiro commit

---
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -0,0 +1,44 @@
+import { FormControl } from '@angular/forms';
+import { Item, LivrosResultado } from './../../models/interfaces';
+import { Component } from '@angular/core';
+import { catchError, debounceTime, distinctUntilChanged, filter, map, of, switchMap, throwError } from 'rxjs';
+import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
+import { LivroService } from 'src/app/service/livro.service';
+
+const PAUSA = 300
+
+@Component({
+  selector: 'app-lista-livros',
+  templateUrl: './lista-livros.component.html',
+  styleUrls: ['./lista-livros.component.css']
+})
+export class ListaLivrosComponent{
+
+  campoBusca = new FormControl();
+  mensagemErro = '';
+  livrosResultado: LivrosResultado;
+
+  constructor(private service: LivroService) { }
+
+  livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
+    filter((valorDigitado) => valorDigitado.length >= 3),
+    distinctUntilChanged(),
+    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+    map(resultado => this.livrosResultado = resultado),
+    map(resultado => resultado.items ?? []),
+    map( items =>  this.livrosResultadoParaLivros(items)),
+    catchError(erro => {
+      return throwError(() => new Error(this.mensagemErro = 'Ops, ocorreu um erro. Recarregue a aplicação'))
+    })
+  )
+
+  livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[]{
+    return items.map(item => {
+      return new LivroVolumeInfo(item)
+    })
+  }
+}
+
+
+
